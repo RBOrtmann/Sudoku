80fdf1750374d27c20cee1718fad25157d809d12
jram99 <RAMSEYERJE17@5CG73109VD.GCC.edu>
2018-12-02 20:14:28 -0500

    Test getcell

--- src/sudokuPackage/BoardTest.java
+++ src/sudokuPackage/BoardTest.java
@@ -5,7 +5,45 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
+/*
+ * 	@Test
+	public void testGetCell() {
+		String boardtest = "000000000\n"
+				+ "050000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n";
+		Board b = new Board(boardtest);
+		assertEquals(5,b.getCell(1, 1));
+	}
 
+	@Test
+	public void testGetCells() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeCell() {
+		String boardtest = "000000000/n"
+				+ "050000000/n"
+				+ "000000000/n"
+				+ "000000000/n"
+				+ "000000000/n"
+				+ "000000000/n"
+				+ "000000000/n"
+				+ "000000000/n"
+				+ "000000000/n";
+		Board b = new Board(boardtest);
+		b.changeCell(0, 0, 3);
+		assertEquals(3,b.getCell(1, 1));
+		//assertEquals(3,b.changeCell(0, 0, 3));
+		//Assert.fail();
+	}
+ */
 	@Test
 	void testBoard() {
 		fail("Not yet implemented");
@@ -18,6 +56,17 @@ class BoardTest {
 
 	@Test
 	void testGetCell() {
+		String boardtest = "000000000\n"
+				+ "050000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n"
+				+ "000000000\n";
+		Board b = new Board(boardtest);
+		assertEquals(5,b.getCell(1, 1));
 		fail("Not yet implemented");
 	}
 
